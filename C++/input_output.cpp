f68552a682efbd20a3ecbd44ba21a44956b2cec2
jagat <nir.j@husky.neu.edu>
2020-05-02 14:21:55 -0400

    first_commit

---
+++ C++/input_output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+ string mystr;
+ float price=0;
+ //int quantity=0;
+ cout<<"Enter price:";
+ cin >> price;
+ return 0;
+}
+
