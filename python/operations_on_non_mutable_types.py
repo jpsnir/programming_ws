773f67a94f287999528553ead587108debe78ade
jagat <nir.j@husky.neu.edu>
2020-05-12 14:07:50 -0400

    added more template tutorials

---
+++ python/operations_on_non_mutable_types.py
@@ -0,0 +1,4 @@
+# Tutorial for mutable types and defines certain properties
+# related to common algorithms about mutable types.
+# profiles and compares efficieny
+
---
+++ python/sys_and_os_commands.py
@@ -0,0 +1,3 @@
+#Tutorial to learn more about using existing operating system commands
+#Reading files,writing files, and so on. 
+
