02ce70a50adf438d2560eee21062c80cb460394c
jagatpreet <nir.j@northeastern.edu>
2023-09-05 16:53:54 -0400

    added tutorial on pointers

---
+++ c++/pointer_basics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+int main(void){
+    std::cout << " Hello, this is a primer on pointers" << std::endl;
+
+    // https://www.geeksforgeeks.org/c-pointer-to-pointer-double-pointer/
+    // 1. cover pointers and address
+    // 2. cover double pointer examples
+    // 3. cover function calls and return types with pointers
+    // 4. double pointers as multi-dimensional arrays; initializing 2d array
+    //    with pointer approach.
+    // 5. Test for contiguous memory locations in a 1d - array, 2d array, etc.
+
+    // 6. function pointers - https://www.geeksforgeeks.org/function-pointer-in-c/
+    //
+}
