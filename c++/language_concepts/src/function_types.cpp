a9e337d4319a17f8e1f3a87bc623f64b428776cd
jagatpreet <nir.j@northeastern.edu>
2023-10-10 12:15:27 -0400

    added different implementations of using functions

---
+++ c++/language_concepts/src/function_types.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <functional>
+#include <cmath>
+
+/* Understanding the different implementations of
+ * function, function pointer, functional,
+ */
+
+double compute_square(double x){
+    return pow(x,2);
+}
+
+class Exponent{
+    public:
+        Exponent()=default;
+        double operator ()(double x){
+            return pow(x,2);
+        }
+        double square(double x){
+            return pow(x,2);
+        }
+};
+
+int main(){
+    auto exp_ptr = new Exponent();
+    Exponent *square_ptr = new Exponent();
+    // raw c style function pointer
+    // functions are stored in binary
+    // with the ampersand we are storing the memory location of the instruction
+    // that will be stored.
+    double (*compute_square_ptr)(double) = &compute_square;
+    auto func = &compute_square;
+    // function pointers with classes
+    double (Exponent::*compute_square_functional)(double) = &Exponent::square;
+    printf("Square of 10 (raw function pointer) = %f\n", (*compute_square_ptr)(10));
+    printf("Square of 10 (using auto) = %f\n", func(10));
+    //using function pointers of methods with objects.
+    printf("Square of 10 (method pointer) = %f\n", (exp_ptr->*compute_square_functional)(10));
+    std::cout <<" Square of 10 (functor with operator overloading) " << (*square_ptr)(10) << std::endl;
+    std::cout <<" Square of 10 (functor with operator overloading) " << (*exp_ptr)(10) << std::endl;
+
+    // std function library
+    std::function<double(double)> square;
+    square = compute_square;
+
+    // std function accepting
+    std::cout << " Square of 10 (std::function) :" << square(10) << std::endl;
+    delete exp_ptr;
+    delete square_ptr;
+}
