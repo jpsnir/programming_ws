41b65a8b93e989a96f372d6885e73dfa4a6f4068
jagatpreet <nir.j@northeastern.edu>
2023-10-30 17:56:50 -0400

    binary tree implemented

--- algorithms/src/tree_ds.cpp
+++ algorithms/src/tree_ds.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
 #include <type_traits>
 #include <vector>
+#include <string>
+#include <ostream>
+#include <sstream>
+#include <cassert>
+
 /*
  * The class describes how to construct a tree
  * The definition of each node is given and associated operations are coded.
@@ -18,6 +24,41 @@ public:
     std::vector<TreeNode*> child_node_ptrs;
     TreeNode(const int data, const int number_of_children){
         this->data = data;
-        this->child_node_ptrs = std::vector<TreeNode*>(number_of_children);
+        this->child_node_ptrs = std::vector<TreeNode*>(number_of_children,nullptr);
+    }
+    void assign_children(const std::vector<TreeNode*> nodes){
+        assert(nodes.size() == child_node_ptrs.size());
+        for (int i = 0; i < child_node_ptrs.size(); i++)
+            child_node_ptrs[i] = nodes[i];
+    }
+    void list_all_children(){
+        for (int i=0; i < this->child_node_ptrs.size(); i++){
+            std::stringstream val;
+            if (child_node_ptrs[i] == nullptr)
+                val <<  "null";
+            else
+                val << child_node_ptrs[i]->data;
+            std::string output;
+            val >> output;
+            std::cout << "child = " << i << " - " << output << std::endl;
+        }
     }
 };
+
+
+// binary tree has 2 child nodes, so each treeNode(data, 2)
+// Ternary tree has 3 child nodes, so each treeNode is defined by (data, 3)
+//
+//
+//
+int main(){
+    TreeNode* root= new TreeNode(10, 2);
+    std::cout << " Before assigning childre " << std::endl;
+    root->list_all_children();
+    std::vector<TreeNode*> ch_node_ptrs(2);
+    ch_node_ptrs[0] = new TreeNode(10,2);
+    ch_node_ptrs[1] = new TreeNode(10,2);
+    std::cout << " After assigning childre " << std::endl;
+    root->assign_children(ch_node_ptrs);
+    root->list_all_children();
+}
