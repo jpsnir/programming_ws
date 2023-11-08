b3cc20432468792cf31dd30a78bfb1708d5eedff
jagatpreet <nir.j@northeastern.edu>
2023-11-08 12:56:42 -0500

    max min using recursion

---
+++ algorithms/src/recursion/min_max.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <array>
+
+/* Recursion depends on dividing the problem upto a point that we can consider
+ * only single problem and incremental step.
+ * If we have max from an array of n-1 numbers and nth number, then we have to just compare
+ * them to find the maximum number.
+ *
+ * Until we reach a single number, the max is itself
+ *
+ */
+int max_using_recursion(int* array, int index){
+
+    int max;
+    if(index==0)
+        return array[0];
+    int max_subset = max_using_recursion(array, index -1);
+    //array[index] is the last element of the array
+    max = array[index] > max_subset ? array[index]: max_subset;
+    return max;
+}
+
+int min_using_recursion(int* array, int index){
+
+    int min;
+    if(index==0)
+        return array[0];
+    int min_subset = min_using_recursion(array, index -1);
+    //array[index] is the last element of the array
+    min = array[index] < min_subset ? array[index]: min_subset;
+    return min;
+}
+int main(){
+    int numbers[]  = { 1,4,3,-5, -4, 8, 6,10,12,100};
+    int length = sizeof(numbers)/sizeof(int);
+    int max = max_using_recursion(numbers, length-1);
+    printf("Maximum number = %d \n", max);
+    int min = min_using_recursion(numbers, length-1);
+    printf("Minimum number = %d \n", min);
+    return 0;
+
+}
