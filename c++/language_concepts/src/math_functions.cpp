9b294d4f173f178eb1681968fd592246b9630552
jagatpreet <nir.j@northeastern.edu>
2024-01-30 14:14:28 -0500

    updated different repositories

---
+++ c++/language_concepts/src/math_functions.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <iostream>
+
+int main(){
+    int b = 2;
+    float d = b*b;
+    std::cout << d  << std::endl;
+    b = -3;
+    d = b*b;
+    std::cout << d << std::endl;
+}
---
+++ c++/language_concepts/src/test_code.cpp
@@ -0,0 +1,12 @@
+/*
+ * Project: src
+ * Description:
+ * File Created: Saturday, 6th January 2024 12:24:39 pm
+ * Author: Jagatpreet (nir.j@northeastern.edu)
+ * -----
+ * Last Modified: Saturday, 6th January 2024 12:24:39 pm
+ * Modified By: Jagatpreet (nir.j@northeastern.edu>)
+ * -----
+ * Copyright <<projectCreationYear>> - 2024 Northeastern University Field Robotics Lab, Northeastern University Field Robotics Lab
+ */
+
