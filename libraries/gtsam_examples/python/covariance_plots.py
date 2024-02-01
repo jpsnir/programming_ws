30f45ea2f3a45044dbd9efe1586daf2b059b5392
jagatpreet <nir.j@northeastern.edu>
2024-02-01 16:38:06 -0500

    added gtsam imu factor example

--- libraries/gtsam_examples/python/covariance_plots.py
+++ libraries/gtsam_examples/python/covariance_plots.py
@@ -3,6 +3,156 @@ from matplotlib import pyplot as plt
 from numpy.testing import assert_allclose
 from matplotlib import patches
 from mpl_toolkits.mplot3d import Axes3D
+import gtsam
+
+
+def plot_covariance(cov: np.ndarray):
+    U, S, V = np.linalg.svd(cov)
+
+    assert S.shape == (dim,), f"Shape of S is {S.shape}"
+    # assert_allclose(s**2, S)
+
+    print(f"Covariance matrix = {AtA}")
+    origin = np.array([x_c, y_c, z_c])
+    confidence = 1.96  # 95% confidence
+    # get the standard deviation along principal directions
+    if dim == 6:
+        r1, r2, r3 = confidence * np.sqrt(S[3:6])
+    else:
+        r1, r2, r3 = confidence * np.sqrt(S)
+
+    # make ellipsoid
+    # to include the last value add 1.
+    th = np.linspace(0, 2 * np.pi, N + 1)
+    phi = np.linspace(-np.pi / 2, np.pi / 2, N + 1)
+
+    # for each element of th we will rotate around an ellipse.
+    # coordinates obey ellipsoid equation:
+    # x^2/r1^2 + y^2/r2^2 + z^2/r3^2 = 1
+    x = r1 * np.outer(np.cos(th), np.cos(phi)).T
+    y = r2 * np.outer(np.sin(th), np.cos(phi)).T
+    z = r3 * np.outer(np.ones_like(th), np.sin(phi)).T
+
+    print(f"shape of x = {x.shape}, y = {y.shape}, z = {z.shape}")
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection="3d")
+    # ax.plot_wireframe(x, y, z)
+    # plt.show()
+
+    # rotate the covariance ellipse:
+    data = np.kron(U[:, 0:1], x) + np.kron(U[:, 1:2], y) + np.kron(U[:, 2:3], z)
+    print(f"rotation matrix = {U}")
+    print(data.shape)
+    n = data.shape[1]
+    x, y, z = data[0:n, :], data[n : 2 * n, :], data[2 * n :, :]
+    fig2 = plt.figure()
+    ax = fig2.add_subplot(projection="3d")
+    print(f"shape of x = {x.shape}, y = {y.shape}, z = {z.shape}")
+    ax.plot_wireframe(x, y, z)
+
+
+def set_axes_equal(ax):
+    """
+    Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    """
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5 * max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+
+def plot_pose_covariance(pose: gtsam.Pose3, cov: np.ndarray, axis_length: float = 0.1):
+    w_R_p = pose.rotation().matrix()
+    origin = pose.translation()
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    # unit vector along x axis is w_R_p[:, 0]
+    x_axis = origin + w_R_p[:, 0] * axis_length
+    line = np.append(origin[np.newaxis], x_axis[np.newaxis], axis=0)
+    xaxis = w_R_p[:, 0].ravel() * axis_length  # flattening just in case
+    yaxis = w_R_p[:, 1].ravel() * axis_length
+    zaxis = w_R_p[:, 2].ravel() * axis_length
+    y_axis = origin + w_R_p[:, 1] * axis_length
+    line = np.append(origin[np.newaxis], y_axis[np.newaxis], axis=0)
+    ax.plot(line[:, 0], line[:, 1], line[:, 2], "g-")
+
+    z_axis = origin + w_R_p[:, 2] * axis_length
+    line = np.append(origin[np.newaxis], z_axis[np.newaxis], axis=0)
+    ax.plot(line[:, 0], line[:, 1], line[:, 2], "b-")
+    print(f"dot product = {xaxis} {yaxis} - {xaxis.dot(yaxis)}")
+
+    ax.plot(line[:, 0], line[:, 1], line[:, 2])
+    ax.quiver(
+        origin[0],
+        origin[1],
+        origin[2],
+        xaxis[0],
+        xaxis[1],
+        xaxis[2],
+        color="r",
+    )
+    ax.quiver(
+        origin[0],
+        origin[1],
+        origin[2],
+        yaxis[0],
+        yaxis[1],
+        yaxis[2],
+        color="g",
+    )
+    ax.quiver(
+        origin[0],
+        origin[1],
+        origin[2],
+        zaxis[0],
+        zaxis[1],
+        zaxis[2],
+        color="b",
+    )
+
+    P_position = cov[3:6, 3:6]
+    plot_covariance(P_position)
+    # using the definition of covariance Cov(x) = E((x-x_mean)(x-x_mean)^T)
+    # Cov(x_w) = E(R(x-x_mean)(x-x_mean)^T R^T)
+    # Cov(x_w) = R cov(x_p) R^T
+    gP = w_R_p @ P_position @ w_R_p.T
+    plot_covariance(P_position)
+    # perpective difference can show that it does not match.
+    set_axes_equal(ax)
+    plt.show()
+
+
+# usage of quiver function
+def plot_arrow():
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    # x, y and z coordinates have to be specified to determine the start of
+    # arrow locations.
+    # U,V , W are directions of the arrows. each element in u , v and w
+    # gives us a direction vector. In this example it is pointing in the direction of
+    # x axis.
+    ax.quiver([1, 1.2, 1.4], [0, 0, 0], [0, 0, 0], 0.1, 0, 0, linewidth=2)
+    plt.show()
+
 
 # get any random matrix
 x_c, y_c, z_c = 0, 0, 0
@@ -15,47 +165,15 @@ AtA = A.transpose() @ A
 
 u, s, v = np.linalg.svd(A)
 cov = AtA
-U, S, V = np.linalg.svd(cov)
-
-assert S.shape == (dim,), f"Shape of S is {S.shape}"
-assert_allclose(s**2, S)
-
-print(f"Covariance matrix = {AtA}")
-origin = np.array([x_c, y_c, z_c])
-confidence = 1.96  # 95% confidence
-# get the standard deviation along principal directions
-if dim == 6:
-    r1, r2, r3 = confidence * np.sqrt(S[3:6])
-else:
-    r1, r2, r3 = confidence * np.sqrt(S)
-
-
-# make ellipsoid
-# to include the last value add 1.
-th = np.linspace(0, 2 * np.pi, N + 1)
-phi = np.linspace(-np.pi / 2, np.pi / 2, N + 1)
-
-# for each element of th we will rotate around an ellipse.
-# coordinates obey ellipsoid equation:
-# x^2/r1^2 + y^2/r2^2 + z^2/r3^2 = 1
-x = r1 * np.outer(np.cos(th), np.cos(phi)).T
-y = r2 * np.outer(np.sin(th), np.cos(phi)).T
-z = r3 * np.outer(np.ones_like(th), np.sin(phi)).T
-
-print(f"shape of x = {x.shape}, y = {y.shape}, z = {z.shape}")
-fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
-ax.plot_wireframe(x, y, z)
+plot_covariance(cov)
 plt.show()
-
-# rotate the covariance ellipse:
-data = np.kron(U[:, 0:1], x) + np.kron(U[:, 1:2], y) + np.kron(U[:, 2:3], z)
-print(f'rotation matrix = {U}')
-print(data.shape)
-n = data.shape[1]
-x, y, z = data[0:n, :], data[n : 2 * n, :], data[2 * n :, :]
-fig2 = plt.figure()
-ax = fig2.add_subplot(projection="3d")
-print(f"shape of x = {x.shape}, y = {y.shape}, z = {z.shape}")
-ax.plot_wireframe(x, y, z)
-plt.show()
+theta = np.pi / 3
+R = np.array(
+    [[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]]
+)
+rot = gtsam.Rot3(R=R)
+pose = gtsam.Pose3(r=rot, t=np.array([0, 0, 0]))
+A = np.random.randn(6, 6)
+AtA = A.T @ A
+cov = AtA
+plot_pose_covariance(pose, cov)
--- libraries/gtsam_examples/python/imu_factor.py
+++ libraries/gtsam_examples/python/imu_factor.py
@@ -8,32 +8,37 @@ from gtsam.utils.plot import plot_pose3
 from mpl_toolkits.mplot3d import Axes3D
 from gtsam.examples.PreintegrationExample import POSES_FIG, PreintegrationExample
 
+# from gtsam.examples.ImuFactorExample import ImuFactorExample as ImuFactorExampleOrig
 import gtsam
+import signal
+import sys
 
-BIAS_KEY= B(0)
+BIAS_KEY = B(0)
 GRAVITY = 9.81
 
 np.set_printoptions(precision=3, suppress=True)
 
+def signal_handler(sig, frame):
+    print("exiting")
+    sys.exit(0)
+
 def parse_args() -> argparse.Namespace:
-    
     parser = argparse.ArgumentParser("Imu Factor")
-    parser.add_argument("--twist_scenario", 
-                        default = "sick_twist",
-                        choices=["zero_twist", "forward_twist", "loop_twist", "sick_twist"])
-    parser.add_argument("--time", "-T",
-                        default=12,
-                        type=int,
-                        help="Total navigation time in seconds")
-    parser.add_argument("--compute_covariances",
-                        default=False,
-                        action='store_true')
-    parser.add_argument("--verbose", default=False, action='store_true')
+    parser.add_argument(
+        "--twist_scenario",
+        default="sick_twist",
+        choices=["zero_twist", "forward_twist", "loop_twist", "sick_twist"],
+    )
+    parser.add_argument(
+        "--time", "-T", default=12, type=int, help="Total navigation time in seconds"
+    )
+    parser.add_argument("--compute_covariances", default=False, action="store_true")
+    parser.add_argument("--verbose", default=False, action="store_true")
     args = parser.parse_args()
     return args
 
+
 class ImuFactorExample(PreintegrationExample):
-    
     def __init__(self, twist_scenario: str = "sick_twist"):
         self.velocity = np.array([2, 0, 0])
         self.priorNoise = gtsam.noiseModel.Isotropic.Sigma(6, 0.1)
@@ -42,10 +47,139 @@ class ImuFactorExample(PreintegrationExample):
         twist_scenarios = dict(
             zero_twist=(np.zeros(3), np.zeros(3)),
             forward_twist=(np.zeros(3), self.velocity),
-            loop_twist = (np.array([0, np.radians(30), self.velocity])),
-            sick_twist = (np.array([np.radians(30), -np.radians(30), 0]), self.velocity)
+            loop_twist=(np.array([0, np.radians(30), 0]), self.velocity),
+            sick_twist=(np.array([np.radians(30), -np.radians(30), 0]), self.velocity),
         )
 
         accBias = np.array([-0.3, 0.1, 0.2])
         gyroBias = np.array([0.1, 0.3, -0.1])
+        bias = gtsam.imuBias.ConstantBias(accBias, gyroBias)
+        params = gtsam.PreintegrationParams.MakeSharedU(GRAVITY)
 
+        gyro_sigma = 1e-3
+        accel_sigma = 1e-3
+        I_3x3 = np.eye(3)
+        params.setGyroscopeCovariance(gyro_sigma**2 * I_3x3)
+        params.setAccelerometerCovariance(accel_sigma**2 * I_3x3)
+        params.setIntegrationCovariance(1e-7**2 * I_3x3)
+        dt = 1e-2
+        super(ImuFactorExample, self).__init__(
+            twist_scenarios[twist_scenario], bias, params, dt
+        )
+
+    def addPrior(self, i: int, graph: gtsam.NonlinearFactorGraph):
+        state = self.scenario.navState(i)
+        graph.push_back(gtsam.PriorFactorPose3(X(i), state.pose(), self.priorNoise))
+        graph.push_back(gtsam.PriorFactorVector(V(i), state.velocity(), self.velNoise))
+
+    def optimize(self, graph: gtsam.NonlinearFactorGraph, initial: gtsam.Values):
+        params = gtsam.LevenbergMarquardtParams()
+        params.setVerbosityLM("SUMMARY")
+        optimizer = gtsam.LevenbergMarquardtOptimizer(graph, initial, params)
+        result = optimizer.optimize()
+        return result
+
+    def plot(
+        self,
+        values: gtsam.Values,
+        title: str = "Estimated Trajectory",
+        fignum: int = POSES_FIG + 1,
+        show: bool = False,
+    ):
+        i = 0
+        while values.exists(X(i)):
+            pose_i = values.atPose3(X(i))
+            plot_pose3(fignum, pose_i, 1)
+            i += 1
+
+        plt.title(title)
+        gtsam.utils.plot.set_axes_equal(fignum)
+        print("Bias Values", values.atConstantBias(BIAS_KEY))
+        plt.ioff()
+
+        if show:
+            plt.show()
+
+    def run(self, T: int = 12, compute_covariances: bool = False, verbose: bool = True):
+        graph = gtsam.NonlinearFactorGraph()
+        pim = gtsam.PreintegratedImuMeasurements(self.params, self.actualBias)
+        num_poses = T
+        initial = gtsam.Values()
+        initial.insert(BIAS_KEY, self.actualBias)
+        i = 0
+        initial_state_i = self.scenario.navState(0)
+        initial.insert(X(i), initial_state_i.pose())
+        initial.insert(V(i), initial_state_i.velocity())
+
+        self.addPrior(0, graph)
+        #print(list(enumerate(np.arange(0, T, 1e-2))))
+        for k, t in enumerate(np.arange(0, T, 1e-2)):
+            measuredOmega = self.runner.measuredAngularVelocity(t)
+            measuredAcc = self.runner.measuredSpecificForce(t)
+            pim.integrateMeasurement(measuredAcc, measuredOmega, self.dt)
+            if k % 10 == 0:
+                self.plotImu(t, measuredOmega, measuredAcc)
+                plt.pause(0.1)
+
+            if (k + 1) % int(1 / self.dt) == 0:
+                self.plotGroundTruthPose(t, scale=1)
+                plt.title("ground truth trajecotry")
+                #print(f"inserting factor {i}")
+                factor = gtsam.ImuFactor(X(i), V(i), X(i + 1), V(i + 1), BIAS_KEY, pim)
+                graph.push_back(factor)
+                print("factor pushed")
+                if verbose:
+                    print(factor)
+                    print(
+                        f"Predicted state at {t + self.dt} : \n {pim.predict(initial_state_i, self.actualBias)}"
+                    )
+                pim.resetIntegration()
+
+                rotationNoise = gtsam.Rot3.Expmap(np.random.randn(3) * 0.1)
+                translationNoise = gtsam.Point3(np.random.randn(3) * 1)
+                poseNoise = gtsam.Pose3(rotationNoise, translationNoise)
+
+                actual_state_i = self.scenario.navState(t + self.dt)
+                print(f"Actual state at {t + self.dt}  \n {actual_state_i}")
+                initial_state_i = actual_state_i
+
+                noisy_state_i = gtsam.NavState(
+                    actual_state_i.pose().compose(poseNoise),
+                    actual_state_i.velocity() + np.random.randn(3) * 0.1,
+                )
+                initial.insert(X(i + 1), noisy_state_i.pose())
+                initial.insert(V(i + 1), noisy_state_i.velocity())
+                i += 1
+
+        print(t)
+        print(f"optmizing here")
+        self.addPrior(num_poses - 1, graph)
+        initial.print("Initial values:")
+        result = self.optimize(graph, initial)
+        result.print("Optmized values")
+        print("-----------")
+        print(graph.error(initial))
+        print(graph.error(result))
+        print("------------------")
+
+        if compute_covariances:
+            marginals = gtsam.marginalCovariances(BIAS_KEY)
+            print("Covariance on bias:\n", marginals.marginalCovariance(BIAS_KEY))
+            for i in range(num_poses):
+                print(
+                    f"Covariance on pose {i} : \n {marginals.marginalCovariance(X(i))}"
+                )
+                print(
+                    "Covariance on vel {}:\n{}\n".format(
+                        i, marginals.marginalCovariance(V(i))
+                    )
+                )
+        self.plot(result, show=True)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    signal.signal(signal.SIGINT, signal_handler)
+    ImuFactorExample(args.twist_scenario).run(
+        args.time, args.compute_covariances, args.verbose
+    )
