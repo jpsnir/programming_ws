b95154368e09ebccb2dd7559ea01c02470e2ee93
jagatpreet <nir.j@northeastern.edu>
2024-02-09 06:48:05 -0500

    input output example

---
+++ c++/language_concepts/src/basics/input_output.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+string s;
+int n, a, b, c;
+cin >> s >> n;
+
+cout << s << " " << n << endl;
+
+cin.clear();
+cin >> a >> b >> c;
+cout << a + b + c << endl;
+}
