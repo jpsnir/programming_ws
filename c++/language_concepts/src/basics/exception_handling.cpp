b7624283cc6fddb4e44749e82ff45a1284227286
jagatpreet <nir.j@northeastern.edu>
2024-02-10 13:06:14 -0500

    updated with comments to add more info

--- c++/language_concepts/src/basics/exception_handling.cpp
+++ c++/language_concepts/src/basics/exception_handling.cpp
@@ -46,6 +46,10 @@ int main(int argc, char* argv[]){
     }
 
     // scenario 2
+    // you can throw exceptions of any intrinsic types,
+    // but to catch those we need to have similar catch statements
+    // with same type.
+    // Also see the use of catch(...) to catch anything
     try{
         if (denominator == 0)
             throw -1;
@@ -56,16 +60,36 @@ int main(int argc, char* argv[]){
     catch (int n){
         cout << " I catch int exceptions: " << n << endl;
     }
+    // can be used in very critical situations where the system is not yet
+    // completely known and under test. This reflects lack of knowledge of the
+    // system.
     catch(...){
         cout << " I catch all exceptions" << endl;
     }
 
     // scenario 3:
+    // This scenarios show cases that any object that are declared within the
+    // try block are destroyed on stack before catch statements are executed.
+    // This is important to know as it can lead to memory leaks and also cause
+    // other problems.
+    //
+    //
+
+    Test t_out;
+    Test* t_ptr;
     try{
+        // test declared in scope of try will be destroyed
         Test t;
+        auto ptr = new Test();
+        // just to prevent memory leak we store it in another variable
+        // ptr will be destroyed as a pointer but the object on heap will not be
+        t_ptr = ptr;
         throw runtime_error("Failed exceuction");
     }
     catch(exception e){
         cout << e.what() << endl;
     }
+    cout << " End of program" << endl;
+    delete t_ptr;
+    return 0;
 }
