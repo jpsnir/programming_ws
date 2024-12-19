e85b906a4fea153d0898aca024b22544d7768487
Jagatpreet <nir.j@northeastern.edu>
2024-12-19 14:25:15 -0500

    srand and rand functionality

--- c++/language_concepts/src/random_number_generator.cpp
+++ c++/language_concepts/src/random_number_generator.cpp
@@ -7,23 +7,29 @@
 using namespace std;
 
 float random(float min, float max){
-    srand(time(0));
-    
+    return float(rand())/RAND_MAX;
 }
 
 int main(){
     float min = 0, max = 1;
+    srand(time(0));
     std::random_device rd;
     std::default_random_engine generator(rd());
     std::uniform_real_distribution<float> u_dist(min, max);
     auto n = std::bind(u_dist, generator);
-    for (int i = 0; i < 10; i ++){
-        time_t t;
-        cout << "time = " << time(0) << " delta offset = " << t << endl;
-        usleep(1000000);
-    }
+    
+    time_t t;
+    time(&t);
+    cout << "time function with null argument = " << time(0) << " t = " << t << endl;
+    
 
+    cout << " ***************************** " << endl;
     for (int i = 0; i < 10; i++){
         cout << "Value = " << n() << endl;
+    }
+
+    cout << " ***************************** " << endl;
+    for (int i = 0; i < 10; i++){
+        cout << "Value = " << random(min, max) << endl;
     }
 }
